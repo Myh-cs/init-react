1dd678097dc9183e915f7d36d4c1b7a8d97d3066
yuhang.meng <yuhang.meng@CNyuhang.meng>
2019-07-29 09:51:39 +0800

    api调用式国际化

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, injectIntl, intlShape } from 'react-intl';
+
 import { connect } from 'react-redux';
 import ProductsContainer from './ProductsContainer'
 import CartContainer from './CartContainer'
@@ -8,7 +9,7 @@ import { changeLang } from '../redux/actions';
 
 
 
-const App = ({ changeLang }) => (
+const App = ({ changeLang, intl: { formatMessage } }) => (
   <div>
     <h2>Shopping Cart Example</h2>
     <hr />
@@ -16,16 +17,19 @@ const App = ({ changeLang }) => (
     <hr />
     <CartContainer />
     <hr />
+    {formatMessage({ id: "name" }, { name: '国际化' })}
+    <hr/>
+    
     <FormattedMessage id='name' values={{
       name: <button onClick={changeLang} > {'国际化'}</button>
     }} />
     <FormattedMessage id="hello" tagName="h2" />
     <FormattedMessage id='hello'>
       {(txt) => (
-        <input type="button"value={txt} />
+        <input type="button" value={txt} />
       )}
     </FormattedMessage>
   </div>
 )
 
-export default connect(() => ({}), { changeLang })(App)
+export default connect(() => ({}), { changeLang })(injectIntl(App));
