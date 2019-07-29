cb96e91bf9646723521abe03c2aba79ea0e81c17
yuhang.meng <yuhang.meng@CNyuhang.meng>
2019-07-29 10:42:15 +0800

    懒加载

--- src/router.js
+++ src/router.js
@@ -1,1 +1,7 @@
-// build router APP
+import Loadable from 'react-loadable';
+// import Loading from './my-loading-component'; // 国际化
+const loading = () => <span>loading</span>
+const LoadableComponent = Loadable({
+    loader: () => import('./pages/Home'),
+    loading: Loading,
+});
