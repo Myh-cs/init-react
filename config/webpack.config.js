90c2b607eb64dddbb7f6268a462fbc703fbf6745
RuiLiu <rui.liu@RuiLiudeMacBook-Pro.local>
2019-08-09 11:15:41 +0800

    增加eslint检查配置

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -44,7 +44,6 @@ const lessRegex = /\.less$/;
 const lessModuleRegex = /\.module\.less$/
 const sassRegex = /\.(scss|sass)$/;
 const sassModuleRegex = /\.module\.(scss|sass)$/;
-
 // This is the production and development configuration.
 // It is focused on developer experience, fast rebuilds, and a minimal bundle.
 module.exports = function (webpackEnv) {
@@ -311,7 +310,7 @@ module.exports = function (webpackEnv) {
               options: {
                 formatter: require.resolve('react-dev-utils/eslintFormatter'),
                 eslintPath: require.resolve('eslint'),
-
+                emitWarning: true
               },
               loader: require.resolve('eslint-loader'),
             },
